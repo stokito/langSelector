32a3ae72bd41630761e199fa0f70b49f46c8c293
Sergey Ponomarev <stokito@gmail.com>
2013-11-02 03:23:48 +0200

    reformat

--- grails-app/taglib/com/mfelix/grails/plugins/langSelector/LangSelectorTagLib.groovy
+++ grails-app/taglib/com/mfelix/grails/plugins/langSelector/LangSelectorTagLib.groovy
@@ -2,20 +2,24 @@ package com.mfelix.grails.plugins.langSelector
 
 class LangSelectorTagLib {
     static namespace = 'langs'
+
     def selector = { attrs ->
-        def values = null
+        String defaultLang = attrs.default
+        String langs = attrs.langs
+        String url = attrs.url
+
+        ArrayList<String> values
         try {
-            values = new ArrayList(attrs.langs.split(',').toList())
+            values = new ArrayList(langs.split(',').toList())
         } catch (Exception e) {
             log.error "Error getting value of required attribute 'langs'", e
             throw new Exception("Error getting value of required attribute 'langs'. Accepted value for example is: es,en_US,en")
         }
-        def selected = session["org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE"]
+        Locale selected = session["org.springframework.web.servlet.i18n.SessionLocaleResolver.LOCALE"]
         // if not set in session, get it from attrs
-        selected = selected ? selected : attrs.default?.trim()
-        //if no default is set get default locale
+        selected = selected ? selected : defaultLang?.trim()
+        // if no default is set get default locale
         selected = selected ? selected : Locale.getDefault()
-        String url = attrs.url
         if (url == null) {
             url = request.getRequestURI() + '?'
             def query = request.getQueryString() ? request.getQueryString().replace('lang=' + selected.toString(), '') : ''
@@ -30,20 +34,20 @@ class LangSelectorTagLib {
                 url += '?lang='
             }
         }
-        def supported = StaticConfig.config
-        def flags = [:]
+        Map<String, String> supported = StaticConfig.config
+        Map flags = [:]
         values.each {
-            def language = it.indexOf("_") > 0 ? it.substring(0, it.indexOf("_")) : it
-            def country = it.indexOf("_") > 0 ? it.substring(it.indexOf("_") + 1, it.length()) : supported[language.toLowerCase().trim()]
+            String language = it.indexOf('_') > 0 ? it.substring(0, it.indexOf('_')) : it
+            def country = it.indexOf('_') > 0 ? it.substring(it.indexOf('_') + 1, it.length()) : supported[language.toLowerCase().trim()]
             if (country) {
                 flags[it.trim()] = country.toLowerCase().trim()
             } else {
-                log.error "No country flag found for:" + language + " please check configuration."
+                log.error "No country flag found for: ${language} please check configuration."
             }
         }
-        //distincion de seleccionado o no por defecto estilo opacity
-        def selected_class = ''
-        def not_selected_class = 'opacitiy_not_selected'
+        // distincion de seleccionado o no por defecto estilo opacity
+        String selected_class = ''
+        String not_selected_class = 'opacitiy_not_selected'
 
         out << render(template: '/langSelector/selector', plugin: 'langSelector',
             model: [flags: flags,
