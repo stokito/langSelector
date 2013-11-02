9eefaed3ed748a9ee317be7d6d191e0db59ad61b
Sergey Ponomarev <stokito@gmail.com>
2013-11-02 02:30:24 +0200

    Updated plugin manifest

--- LangSelectorGrailsPlugin.groovy
+++ LangSelectorGrailsPlugin.groovy
@@ -1,50 +1,16 @@
 class LangSelectorGrailsPlugin {
     // the plugin version
-    def version = "0.3"
+    String version = '0.3.1'
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.2.2 > *"
-    // the other plugins this plugin depends on
-    def dependsOn = [:]
-    // resources that are excluded from plugin packaging
-    def pluginExcludes = [
-            "grails-app/views/error.gsp"
-    ]
-
-    // TODO Fill in these fields
-    def author = "Maximiliano Felix"
-    def authorEmail = "maxi.felix@gmail.com"
-    def title = "Language Selector using Grails i18n support."
-    def description = '''\\
+    String grailsVersion = '1.2.2 > *'
+    String author = 'Maximiliano Felix'
+    String authorEmail = 'maxi.felix@gmail.com'
+    String title = 'Language Selector using Grails i18n support.'
+    String description = '''\\
 Simple to use tag, lets you change language of your application. It is based in i18n support provided by Grails.
 '''
-
-    // URL to the plugin's documentation
-    def documentation = "http://grails.org/plugin/lang-selector"
-
-    def doWithWebDescriptor = { xml ->
-        // TODO Implement additions to web.xml (optional), this event occurs before 
-    }
-
-    def doWithSpring = {
-        // TODO Implement runtime spring config (optional)
-    }
-
-    def doWithDynamicMethods = { ctx ->
-        // TODO Implement registering dynamic methods to classes (optional)
-    }
-
-    def doWithApplicationContext = { applicationContext ->
-        // TODO Implement post initialization spring config (optional)
-    }
-
-    def onChange = { event ->
-        // TODO Implement code that is executed when any artefact that this plugin is
-        // watching is modified and reloaded. The event contains: event.source,
-        // event.application, event.manager, event.ctx, and event.plugin.
-    }
-
-    def onConfigChange = { event ->
-        // TODO Implement code that is executed when the project configuration changes.
-        // The event is the same as for 'onChange'.
-    }
+    String documentation = 'http://grails.org/plugin/lang-selector'
+    String license = 'GNU'
+    def issueManagement = [system: 'GitHub', url: 'https://github.com/maxifelix/langSelector/issues']
+    def scm = [url: 'https://github.com/maxifelix/langSelector']
 }
