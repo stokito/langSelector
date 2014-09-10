68c16ca224bed8bcfd6474082d193da11c0589ed
Eric Chauvin <echauvin@webbfontaine.com>
2014-09-10 13:37:28 +0200

    code review

--- grails-app/utils/com/mfelix/grails/plugins/langSelector/StaticConfig.groovy
+++ grails-app/utils/com/mfelix/grails/plugins/langSelector/StaticConfig.groovy
@@ -1,6 +1,6 @@
 package com.mfelix.grails.plugins.langSelector
 
-import grails.util.Holders as CH
+import grails.util.Holders
 
 class StaticConfig {
     /** this static property can be overriden by config */
@@ -21,6 +21,6 @@ class StaticConfig {
 
     //com.mfelix.grails.plugins.langSelector
     static Map<String, String> getConfig() {
-        CH.config.com.mfelix.grails.plugins.langSelector.lang.flags ? CH.config.com.mfelix.grails.plugins.langSelector.lang.flags : LANG_FLAGS
+        Holders.config.com.mfelix.grails.plugins.langSelector.lang.flags ? Holders.config.com.mfelix.grails.plugins.langSelector.lang.flags : LANG_FLAGS
     }
 }
